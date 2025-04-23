a95a5d92e4380b12f57424b6dc348dd6378cfbee
Gavin <kirengaboris07@gmail.com>
2025-04-23 14:37:27 +0200

    Debug of the Server

--- config/db.js
+++ config/db.js
@@ -5,10 +5,12 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      
     });
 
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
+
     console.error("❌ MongoDB connection error:", error.message);
     process.exit(1);
   }
