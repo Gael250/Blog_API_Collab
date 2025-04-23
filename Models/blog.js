99cacd665808d41031b8be963a2eaf04b2a4bfe4
Gael <gaeluwizera250@gmail.com>
2025-04-23 14:39:35 +0200

    Fixing the Routes

--- Models/blog.js
+++ Models/blog.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  
   content: { type: String, required: true },
   category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 }, { timestamps: true });
 
--- Routes/users.js
+++ Routes/users.js
@@ -4,6 +4,7 @@ const userCtrl = require("../controllers/userController");
 const auth = require("../Middleware/auth");
 const roleCheck = require("../Middleware/roleCheck");
 
+
 router.get("/", auth, roleCheck("admin"), userCtrl.getAllUsers);
 router.get("/:id", auth, userCtrl.getUserById);
 router.put("/:id", auth, userCtrl.updateUser);
